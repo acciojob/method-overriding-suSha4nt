cc087bb4a84c4724e293be2e9a8619694d462f1c
suSha4nt <heysusanta@gmail.com>
2026-03-18 23:15:59 +0530

    method overloading completed fifth  attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,25 @@
 package com.driver;
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
-class B extends A {
-    public String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
+
 public class Main {
+
+    // Inner class A
+    public static class A {
+        public String meth() {
+            return "Invoking method from class A";
+        }
+    }
+
+    // Inner class B extends A
+    public static class B extends A {
+        @Override
+        public String meth() {
+            return "Method is overridden in Extended class B";
+        }
+    }
+
     public static void main(String[] args) {
-    B obj = new B();
-    String print = obj.meth();
-    System.out.println(print);
+        B obj = new B();
+        String print = obj.meth();
+        System.out.println(print);
     }
 }
