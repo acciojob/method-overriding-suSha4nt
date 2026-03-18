59158d55dc318eb92b1be0a0bd9de0c23b20d6ed
suSha4nt <heysusanta@gmail.com>
2026-03-18 22:55:19 +0530

    method overloading completed second attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,20 @@
 package com.driver;
 
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+class B extends A{
+@Override
+    public String meth() {
+        return "Method is overridden in Extendend class B";
+   }
+}
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        String ans = obj.meth();
+        System.out.println(ans);
+    }
 }
