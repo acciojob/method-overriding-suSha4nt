7f578a8d153e187233059a477132508c33acd6b8
suSha4nt <heysusanta@gmail.com>
2026-03-18 23:55:02 +0530

    method overloading completed eighth attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,28 @@
-package com.driver;
+// Task 1: Create class A
+class A {
+    String meth() {
+        return "Invoking method from class A";
+    }
+}
 
+// Task 2: Create class B which extends A
+class B extends A {
+
+    // Task 4: Override method meth
+    @Override
+    String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+
+// Task 5: Main class to test everything
 public class Main {
-
-    public static class A {
-        public String meth() {
-            return "Invoking method from class A";
-        }
-    }
-
-    public static class B extends A {
-        @Override
-        public String meth() {
-            return "Method is overridden in Extended class B";
-        }
-    }
-
     public static void main(String[] args) {
+
+        // Task 3: Create object of B
         B obj = new B();
-        String ans = obj.meth();
-        System.out.println(ans);
+
+        // Calling overridden method (Task 5)
+        System.out.println(obj.meth());
     }
 }
