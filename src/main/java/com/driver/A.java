d3b0166b0846d5f4e32b66a7d3d948180387359a
suSha4nt <heysusanta@gmail.com>
2026-03-18 23:50:10 +0530

    method overloading completed seventh attempt

--- src/main/java/com/driver/A.java
+++
@@ -1,7 +0,0 @@
-package com.driver;
-
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
--- src/main/java/com/driver/B.java
+++
@@ -1,8 +0,0 @@
-package com.driver;
-
-class B extends A{
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
