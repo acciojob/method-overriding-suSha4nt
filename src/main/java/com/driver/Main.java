1fa78c9b4c8460d25b8f24e65f03c6a24ab83e65
suSha4nt <heysusanta@gmail.com>
2026-03-20 01:01:54 +0530

    method overloading completed 15th attempt [15]

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,15 +9,15 @@ public class Main {
         public static class B extends A {
             @Override
             public String meth() {
-                return "Method is overridden in Extended class B";
+                return "Method is overridden in Extendend class B";
             }
         }
         public static void main(String[] args) {
             B b = new B();
             System.out.println(b.meth());
 
-            B b2 = new B();
-            System.out.println(b2.meth());
+            B b1 = new B();
+            System.out.println(b1.meth());
 
         }
 }
