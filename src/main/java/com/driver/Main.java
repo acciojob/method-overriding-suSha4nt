9dcdba05ffa562ce5ecdcd302e5131c2c5f870ea
suSha4nt <heysusanta@gmail.com>
2026-03-19 09:38:02 +0530

    method overloading completed 11th attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,3 +1,4 @@
+
 package com.driver;
 
 public class Main {
@@ -16,11 +17,14 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
+        // Task 3: Create an object of class B
         B b = new B();
-        System.out.println(b.meth());
 
-        B b2 = new B();
-        System.out.println(b2.meth());
+        // Call method meth of class A using the object of class B
+//        System.out.println(b.super.meth()); // This will not work in Java. Instead, we can call it directly
+        System.out.println(((A) b).meth()); // This casts b to A to call the superclass method
+
+        // Task 5: Call the overridden method from the object of class B
+        System.out.println(b.meth());
     }
 }
